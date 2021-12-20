faf4fdb96d0b41f8f15a15eaa6efafc3aa8f8f4e
saim14 <saim.islam698@gmail.com>
2021-12-20 10:05:29 +0600

    added tailwindcss

--- components/user.js
+++ components/user.js
@@ -2,10 +2,9 @@ import React from "react";
 
 export default function User({ user }) {
   return (
-    <div key={user.id}>
+    <div className="bg-blue-100 my-2 px-4 py-2" key={user.id}>
       <p>{user.name}</p>
       <p>{user.email}</p>
-      <hr />
     </div>
   );
 }
--- pages/users.js
+++ pages/users.js
@@ -3,9 +3,8 @@ import User from "../components/user";
 
 export default function UserList({ users }) {
   return (
-    <div>
-      <h1>List of Users</h1>
-      <hr />
+    <div className="bg-blue-500 -mt-2">
+      <h1 className="text-3xl m-2 p-2 text-white">List of Users</h1>
       {users.map((user) => (
         <User user={user} />
       ))}
@@ -13,6 +12,7 @@ export default function UserList({ users }) {
   );
 }
 
+// This code run only on the server side.
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
